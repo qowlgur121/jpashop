2dbf22f01f664ca1230587930237ad13d60e37d8
Jihyeok <qowlgur121@gmail.com>
2025-01-16 23:16:07 +0900

    feat: 주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -35,6 +35,9 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
 
+    protected Order(){}
+
+    //==연관관계 메서드==// 즉 연관된 엔티티에 지금의 엔티티를 업데이트 하는 것. 만약 여기서 데이터가 수정되면 다른 곳에서도 그것을 알아야 하니까...
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
@@ -51,4 +54,43 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        // 밑은 관련된 엔티티에도 업데이트를 하게 해줌.
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        // 밑은 관련된 엔티티에도 변화를 업데이트 해주기 위함임. 주문을 했다가 취소하면 상품의 개수가 다시 복구되어야 하니까...
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /** 전체 주문 가격 조회 */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Entity
 @Table(name = "order_item")
@@ -25,4 +26,27 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    protected OrderItem() {} //외부에서 생성 막기 그대신 밑의 메서드는 접근할 수 있도록..
+
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    /** 주문 취소 */
+    public void cancel() {
+        getItem().addStock(count); // 주문했던 수량을 재고에다가 다시 더해주는..
+    }
+
+    //==조회 로직==//
+    /** 주문상품 전체 가격 조회 */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
